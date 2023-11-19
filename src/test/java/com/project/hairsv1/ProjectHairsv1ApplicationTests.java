369c5bd4579c007cd3210290c16bf23bf449ae42
Sebastian Murillo <sebasty06@ouglook.com>
2023-11-18 20:20:51 -0500

    Start project

---
+++ src/test/java/com/project/hairsv1/ProjectHairsv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hairsv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHairsv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
